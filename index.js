830f4a3311f8961452ac1d639803d5c6df02b13a
Ayoub HADDAD <aub.haddad@gmail.com>
2020-03-04 21:29:53 +0100

    add default empty array return to find and findcodebycity

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const countries = require("./countries");
 
 const find = (country, code) => {
   const zip = code.length < 5 ? String("00000" + code).slice(-5) : code
-  return countries[country].codes[zip]
+  return countries[country].codes[zip] || []
 }
 const findCodesByCity = (country, city) => {
-  return countries[country].citiesMap[city.toUpperCase()]
+  return countries[country].citiesMap[city.toUpperCase()] || []
 }
 
 const getAllCities = (country) => {
